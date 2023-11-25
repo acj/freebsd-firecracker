6a54e77f74349b1b8fa2ff1404e8a81ee5ecaf05
Adam Jensen <adam@acj.sh>
2023-11-25 14:16:36 -0500

    Compile with parallelism

--- buildworld.sh
+++ buildworld.sh
@@ -4,5 +4,5 @@ set -e
 
 cd /vagrant
 
-sudo make -C /usr/src buildworld KERNCONF=FIRECRACKER
-sudo make -C /usr/src/release firecracker DESTDIR=$(pwd)
+sudo make -j$(sysctl -n hw.ncpu) -C /usr/src buildworld KERNCONF=FIRECRACKER
+sudo make -j$(sysctl -n hw.ncpu) -C /usr/src/release firecracker DESTDIR=$(pwd)
