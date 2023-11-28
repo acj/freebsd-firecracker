75621762585f7d239688a74c70df57a7f74664c9
Adam Jensen <adam@acj.sh>
2023-11-27 22:32:57 -0500

    Make sure the kernel can find its root filesystem without help

--- buildkernel.sh
+++ buildkernel.sh
@@ -4,6 +4,11 @@ set -e
 
 cd /vagrant
 
+# Without this, we end up at the mountroot prompt when booting the VM
+sudo cat <<END >> /usr/src/sys/amd64/conf/FIRECRACKER
+options ROOTDEVNAME=\"ffs:/dev/vtbd0\"
+END
+
 sudo make -DNO_CLEAN -C /usr/src buildkernel KERNCONF=FIRECRACKER
 sudo make -C /usr/src/release firecracker DESTDIR=$(pwd)
 #sudo chown -R vagrant:vagrant /vagrant
