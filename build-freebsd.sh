cd2778c39ed673474404e44cd79bae25f07c15a6
Adam Jensen <adam@acj.sh>
2023-12-10 21:43:40 -0500

    Use more common ufs FS type

--- build-freebsd.sh
+++ build-freebsd.sh
@@ -6,7 +6,7 @@ cd /vagrant
 
 # Without this, we end up at the mountroot prompt when booting the VM
 cat <<END >> /usr/src/sys/amd64/conf/FIRECRACKER
-options ROOTDEVNAME=\"ffs:/dev/vtbd0\"
+options ROOTDEVNAME=\"ufs:/dev/vtbd0\"
 END
 
 make -j$(sysctl -n hw.ncpu) -C /usr/src buildworld buildkernel KERNCONF=FIRECRACKER
